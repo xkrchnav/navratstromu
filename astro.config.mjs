be2b105887f248701ffbc57474ad275c6c62e3ef
meraajoz <jozef.mera@tietoevry.com>
2026-03-11 10:15:17 +0100

    cloudflare  fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
 import remarkGroupMdxImages from "./src/remark/remarkGroupMdxImages.mjs";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   integrations: [mdx({ remarkPlugins: [remarkGroupMdxImages] })],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare(),
 });
